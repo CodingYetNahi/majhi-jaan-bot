2eae0d491e19c97da6b9b4c6ec8287c3ce46c69d
Prashant <317259075+CodingYetNahi@users.noreply.github.com>
2026-09-06 23:36:11 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  // Replace 'majhi-jaan-bot' with your actual GitHub repo name
-  base: 'majhi-jaan-bot', 
-  build: {
-    outDir: 'dist'
-  }
-});
