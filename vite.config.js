869cd57da20fd5a00f54cc9afa6d261e711c2301
Prashant <317259075+CodingYetNahi@users.noreply.github.com>
2026-09-06 23:17:28 +0530

    Update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // Replace 'majhi-jaan-bot' with your actual GitHub repo name
-  base: '/majhi-jaan-bot/', 
+  base: 'majhi-jaan-bot', 
   build: {
     outDir: 'dist'
   }
